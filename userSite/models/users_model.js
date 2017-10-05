6046955e216b44547222f0fe75f0191ea06f07f5
Rick <kylin792884522@outlook.com>
2017-10-05 17:23:31 +0800

    commit

--- userSite/models/users_model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-    username: { type: String, unique: true },
-    email: String,
-    color: String,
-    hashed_password: String
-});
-mongoose.model('User', UserSchema);
