c603e269bae33bd8f13d86af296bb17bc4eb8dd7
Rick <kylin792884522@outlook.com>
2017-12-04 15:31:30 +0800

    emmm

---
+++ server/logic/bossuser/bossuser_logic.js
@@ -0,0 +1,253 @@
+/**
+ * Created by Ruozi on 8/11/17.
+ */
+
+'use strict';
+
+let Joi = require('joi');
+let ResponseErrorSet = require('../../conf/response_error_set');
+let BossUserDB = require('../../models/bossuser/bossuser_db');
+let IsEmpty = require('is-empty');
+let debug = require('debug')('BOSS_USER');
+let RecordLogic = require('../record_logic');
+let RecordLogger = require('winston').loggers.get('RecordLogger');
+let RecordSet = require('../../conf/record_set');
+
+exports.login = function (params) {
+    return new Promise((resolve, reject) => {
+        _validateLoginParams(params)
+            .then(_getUser)
+            .then(user => {
+
+                RecordLogic.recordOperation({
+                    user_name: params.session.user.user_name,
+                    timestamp: Date.now(),
+                    boss_record: RecordSet.LOGIN
+                })
+                    .catch(err => {
+                        RecordLogger.error(`[${RecordSet.LOGIN}] Record error => ${err.stack}`);
+                    });
+
+                resolve(user);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    });
+};
+
+exports.logout = function (params) {
+    return new Promise((resolve, reject) => {
+        if (!IsEmpty(params.session) && !IsEmpty(params.session.user)) {
+            let userName=params.session.user.user_name;
+            params.session.user = null;
+
+            RecordLogic.recordOperation({
+                user_name: userName,
+                timestamp: Date.now(),
+                boss_record: RecordSet.LOGOUT
+            })
+                .catch(err => {
+                    RecordLogger.error(`[${RecordSet.LOGOUT}] Record error => ${err.stack}`);
+                });
+
+            resolve({});
+        } else {
+            reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.USER_ALREADY_LOGOUT));
+        }
+    });
+};
+
+exports.getUser = function (params) {
+    return new Promise((resolve, reject) => {
+        if (IsEmpty(params.user_name)) {
+            debug(`no params in url, please try again`);
+            reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.USER_PARAMS_ERROR));
+        }
+        BossUserDB.getUserByName(params.user_name)
+        .then(result => {
+            let user = result.Item;
+
+            if (IsEmpty(user)) {
+                reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.USER_NAME_NOT_EXIST));
+            } else {
+                delete user.password;
+                delete user.update_timestamp;
+                delete user.update_date_time;
+                user.auth = JSON.parse(user.auth);
+                resolve(user);
+            }
+        })
+        .catch(err => {
+            reject(err);
+        });
+
+    });
+}
+
+exports.getUserList = function (params) {
+    return new Promise((resolve, reject) => {
+    
+        BossUserDB.getUserList({
+            Limit: params.limit,
+            ExclusiveStartKey: params.startKey,
+        })
+        .then(BossUserDBQueryData => {
+
+            let userItems = BossUserDBQueryData.Items;
+
+            debug(`query users => ${JSON.stringify(userItems, null, 2)}`);
+
+            let formatteduserItems = userItems.map(item => {
+                delete item.password;
+                delete item.update_timestamp;
+                // delete item.update_date_time;
+                item.update_date_time = BossUserDBQueryData.LastEvaluatedKey
+                return item;
+            });
+            resolve({
+                users: formatteduserItems,
+                nextKey: BossUserDBQueryData.LastEvaluatedKey
+            });
+        })
+        .catch(err => {
+            debug(`[Bossuser] get all => ${JSON.stringify(err, null, 2)}`);
+            reject(err);
+        })
+    });
+}
+exports.newUser = function (params) {
+    return new Promise((resolve, reject) => {
+        if(getUserByName(params)!=null){
+            reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.USER_ALREADY_EXIST));
+        }
+        _validateUserParams(params)
+            .then(userItem => {
+                return BossUserDB.addUser(userItem);
+            })
+            .then(userItem => {
+                resolve({
+                        user_name: userItem.user_name
+                });
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+function _getUser(params) {
+    return new Promise((resolve, reject) => {
+        if (!IsEmpty(params.session.user)) {
+            debug(`User session exists. session => ${JSON.stringify(params.session, null, 2)}`);
+            resolve(params.session.user);
+        } else if (IsEmpty(params.user_name) || IsEmpty(params.password)) {
+            debug(`User session not exists. User should login again.`);
+            reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.USER_SESSION_MISSING));
+        } else {
+            debug(`User login. login info => ${JSON.stringify(params, null, 2)}`);
+            BossUserDB.getUserByName(params.user_name)
+                .then(result => {
+                    let user = result.Item;
+
+                    if (IsEmpty(user)) {
+                        reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.LOGIN_USER_NAME_NOT_EXIST));
+                    } else if (params.password !== user.password) {
+                        reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.LOGIN_PASSWORD_INCORRECT));
+                    } else {
+                        delete user.password;
+                        user.auth = JSON.parse(user.auth);
+                        params.session.user = user;
+                        resolve(user);
+                    }
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        }
+    });
+};
+
+function _updateUser (params) {
+    
+};
+    
+function _deleteUser (params) {
+
+};
+
+function _getAllUser (params) {
+    
+};
+    
+function _validateLoginParams(params) {
+    return new Promise((resolve, reject) => {
+
+        let schema = {
+            user_name: Joi.string(),
+            password: Joi.string(),
+            session: Joi.object().required()
+        };
+
+        let options = {
+            convert: false,
+            allowUnknown: true
+        };
+
+        Joi.validate(params, schema, options, (err, value) => {
+            if (err) {
+                logger.error(`Param error, Params => ${JSON.stringify(params)}, err => ${err.stack}`);
+                reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.LOGIN_PARAMS_ERROR));
+            } else {
+                resolve(value);
+            }
+        });
+    });
+}
+
+function _validateNameParams(params) {
+    return new Promise((resolve, reject) => {
+
+        let schema = {
+            user_name: Joi.string(),
+        };
+
+        let options = {
+            convert: false,
+            allowUnknown: true
+        };
+
+        Joi.validate(params, schema, options, (err, value) => {
+            if (err) {
+                logger.error(`Param error, Params => ${JSON.stringify(params)}, err => ${err.stack}`);
+                reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.LOGIN_PARAMS_ERROR));
+            } else {
+                resolve(value);
+            }
+        });
+    });
+}
+function _validateUserParams(params) {
+    return new Promise((resolve, reject) => {
+
+        let schema = {
+            user_name: Joi.string(),
+            password: Joi.string(),
+            auth: Joi.object(),
+        };
+
+        let options = {
+            convert: false,
+            allowUnknown: true
+        };
+
+        Joi.validate(params, schema, options, (err, value) => {
+            if (err) {
+                logger.error(`Param error, Params => ${JSON.stringify(params)}, err => ${err.stack}`);
+                reject(ResponseErrorSet.createResponseError(ResponseErrorSet.BossUserErrorSet.LOGIN_PARAMS_ERROR));
+            } else {
+                resolve(value);
+            }
+        });
+    });
+}
---
+++ server/test/developing/boss_user_test.js
@@ -0,0 +1,318 @@
+'use strict';
+
+let chai = require('chai');
+let expect = require('chai').expect;
+let config = require('config');
+let bossUserData = require('../../dev_scripts/local_db_dev/test_user.json');
+let ResponseErrorSet = require('../../conf/response_error_set');
+const debug = require('debug')('TEST');
+let TestUtils = require('../../utils/test_utils');
+
+chai.use(require('chai-http'));
+chai.use(require('chai-json-schema'));
+
+const requestUrl = config.BaseUrl;
+
+let getAllUserJsonSchema = {
+	type: 'object', 
+	required: ['users'],
+	properties: {
+		users: {
+			type: 'array', 
+			items: {
+				type: 'object', 
+				required: [	'user_name','auth'],
+				properties: {
+					user_name: {type: 'string'},
+					auth: {
+						type: 'object', 
+						properties: {
+							Version: {type: 'string'}, 
+							Action: {
+								type: 'array', 
+								items: {type: 'string'}
+							}, 
+							Resource: {
+								type: 'array', 
+								items: {type: 'string'}
+							}  
+						}
+					}
+				}
+			}
+		},
+		nextKey: {type: 'object'}
+	},
+};
+
+let getUserSchema = {
+	type: 'object', 
+	required: ['user_name', 'auth'],
+	properties: {
+		user_name: {type: 'string'},
+		auth: {
+			type: 'object', 
+			properties: {
+				Version: {type: 'string'}, 
+				Action: {
+					type: 'array', 
+					items: {type: 'string'}
+				}, 
+				Resource: {
+					type: 'array', 
+					items: {type: 'string'}
+				}
+			}
+		}
+	}
+};
+
+let returnSchema = {
+	type: 'object',
+	properties: {
+		user: {
+			type: 'object',
+			required: [	'user_name'],
+			properties: {
+			    user_name: {type: 'string'}
+			}
+		}
+	}
+};
+
+describe('Boss Users Get All: ', function(){
+	this.timeout(15000);
+
+	it('should Get all users ', done => {
+		chai.request(requestUrl)
+			.post('/bossuser/userlist')
+			.send({
+				limit: 2
+			})
+			.then(res => {
+				// debug(JSON.stringify(res.body, null, 2));
+				expect(res.body).to.be.jsonSchema(getAllUserJsonSchema);
+				done();
+			})
+			.catch(err => {
+				debug(JSON.stringify(err, null, 2));
+				done(err);
+			});
+	});
+
+});
+
+describe('Boss Users Add: ', function(){
+	this.timeout(15000);
+
+	it('should add a user ', done =>{
+		chai.request(requestUrl)
+			.put('/bossuser/newuser')
+			.send({
+				user_name: 'test_user_a',
+				password: '123454',
+				auth:{
+					VERSION: "2017-09-27",
+					ACTION: ['*'],
+					RESOURCE: [	
+						"order/*",
+						"user/*"
+					]
+				}
+			})
+			.then(res => {
+				expect(res).to.have.status(200);
+				debug(JSON.stringify(res.body, null, 2));
+				expect(res.body).to.be.jsonSchema(returnSchema);
+				done();
+			})
+			.catch(err => {
+				debug(JSON.stringify(err.stack, null, 2));
+				done(err);
+			});
+	});
+  
+	it('should add another user ', done =>{
+		chai.request(requestUrl)
+			.put('/bossuser/newuser')
+			.send({
+				user_name: 'test_user_b',
+				password: '123454',
+				auth:{
+					VERSION: "2017-09-27",
+					ACTION: ['*'],
+					RESOURCE: [	
+						"order/*",
+						"user/*"
+					]
+				}
+			})
+			.then(res => {
+				expect(res).to.have.status(200);
+				expect(res.body).to.be.jsonSchema(returnSchema);
+				done();
+			})
+			.catch(err => {
+				done(err);
+			});
+	});
+
+	it('should add a user who is already exist and return ERROR', done =>{
+		chai.request(requestUrl)
+			.put('/bossuser/newuser')
+			.send({
+				user_name: 'test_user_b',
+				password: '123454',
+				auth:{
+					VERSION: "2017-09-27",
+					ACTION: ['*'],
+					RESOURCE: [	
+						"order/*",
+						"user/*"
+					]
+				}
+			})
+			.end((res,err) => {
+				debug(JSON.stringify(err.stack, null, 2));
+				expect(err).to.have.status(ResponseErrorSet.BossUserErrorSet.USER_ALREADY_EXIST.status);
+				done();
+			});
+	});
+
+});
+
+// describe('Boss Users Update：', function(){
+// 	this.timeout(15000);
+
+// 	it('should update a user ', done =>{
+// 		chai.request(requestUrl)
+// 			.post('/bossuser/updateuser')
+// 			.send({
+// 				user_name: bossUserData.user_name,
+// 				password: '123454',
+// 				auth:{
+// 					VERSION: "2070-02-10",
+// 					ACTION: ['*'],
+// 					RESOURCE: [	
+// 						"order/*",
+// 						"user/*"
+// 					]
+// 				}
+// 			})
+// 			.then(res => {
+// 				expect(res).to.have.status(200);
+// 				expect(res.body).to.be.jsonSchema(returnSchema);
+// 				debug(JSON.stringify(res.body, null, 2));
+// 				done();
+// 			})
+// 			.catch(err => {
+// 				done(err);
+// 			});
+// 	});
+
+// 	it('should update a user who not exists and return ERROR', done =>{
+// 		chai.request(requestUrl)
+// 			.post('/bossuser/updateuser')
+// 			.send({
+// 				user_name: 'test_user_not_exist',
+// 				password: '123454',
+// 				auth:{
+// 					VERSION: "2017-09-27",
+// 					ACTION: ['*'],
+// 					RESOURCE: [	
+// 						"order/*",
+// 						"user/*"
+// 					]
+// 				}
+// 			})
+// 			.end((res,err) => {
+// 				expect(err).to.have.status(ResponseErrorSet.BossUserErrorSet.USER_NAME_NOT_EXIST.status);
+// 				debug(JSON.stringify((err), null, 2));
+// 				done();
+// 			});
+// 	});
+
+// });
+
+describe('Boss Users Get:', function(){
+	this.timeout(15000);
+
+	it('should get one user ', done =>{
+		chai.request(requestUrl)
+			.get('/bossuser/user')
+			.query({
+				user_name: bossUserData.user_name,
+			})
+			.then(res => {
+				expect(res).to.have.status(200);
+				expect(res.body).to.be.jsonSchema(getUserSchema);
+				done();
+			})
+			.catch(err => {
+				done(err);
+				debug(err.stack);
+			});
+	});
+
+	it('should get a Non-exist user and return ERROR', done =>{
+		chai.request(requestUrl)
+			.get('/bossuser/user')
+			.query({
+				user_name:'test_user_not_exist'
+			})
+			.end((res,err) => {
+				expect(err).to.have.status(ResponseErrorSet.BossUserErrorSet.USER_NAME_NOT_EXIST.status);
+				debug(JSON.stringify((err), null, 2));
+				done();
+			});
+	});
+
+});
+
+// describe('Boss Users Delete：', function(){
+// 	this.timeout(15000);
+
+// 	it('should delete a user ', done =>{
+// 		chai.request(requestUrl)
+// 			.delete('/bossuser/deleteuser')
+// 			.send({
+// 				user_name: bossUserData.user_name,
+// 			})
+// 			.then(res => {
+// 				expect(res).to.have.status(200);
+// 				expect(res.body).to.be.jsonSchema(returnSchema);
+// 				done();
+// 			})
+// 			.catch(err => {
+// 				done(err);
+// 			});
+// 	});
+
+// 	it('should delete a user who not exists and return ERROR', done =>{
+// 		chai.request(requestUrl)
+// 			.delete('/bossuser/deleteuser')
+// 			.send({
+// 				user_name: 'test_user_not_exist'
+// 			})
+// 			.end((res,err) => {
+// 				expect(err).to.have.status(ResponseErrorSet.BossUserErrorSet.USER_NAME_NOT_EXIST.status);
+// 				debug(JSON.stringify((err), null, 2));
+// 				done();
+// 			});
+// 	});
+ 
+// 	// todo
+// 	it('should delete a admin return ERROR', done =>{
+// 		chai.request(requestUrl)
+// 			.delete('/bossuser/deleteuser')
+// 			.send({
+// 				user_name: 'admin'
+// 			})
+// 			.end((res,err) => {
+// 				expect(err).to.have.status(ResponseErrorSet.BossUserErrorSet.USER_NAME_NOT_EXIST.status);
+// 				debug(JSON.stringify((err), null, 2));
+// 				done();
+// 			});
+// 	});
+ 
+// });
